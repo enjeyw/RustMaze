f4b9cce61e3d6a047d7a887761970c9989c4749b
Nick Williams <enjeyw@gmail.com>
2021-08-01 15:50:57 +1000

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,104 @@
 use rand::rngs::ThreadRng;
-use std::{thread, time, fmt};
-use std::io::{Stdout, Write, stdout};
+use std::{thread, time};
+use std::io::{ Write, stdout};
 use crossterm::{QueueableCommand, cursor};
-use rand::Rng;
-use rand::{seq::IteratorRandom, thread_rng};
+use rand::{ seq::IteratorRandom, thread_rng};
 
 mod matrix;
-use matrix::{Point, Matrix};
+use matrix::{Point, PointDelta, MazeObj, Matrix};
 
-fn generate_path(rows: usize, cols: usize, starting_col: usize) -> Matrix<Point> {
-    let mut stdout = stdout();
+fn generate_path(rows: usize, cols: usize, starting_col: usize) -> Matrix<MazeObj> {
 
     let mut m = Matrix::new(rows, cols);
 
-    let mut cur_row = 0 as usize;
-    let mut cur_col = starting_col;
+    let mut cur_point = Point{x: 0, y: starting_col};
 
     let mut rng = thread_rng();
 
     for _ in 0..10 {
-        let (next_row, next_col) = fill_next_point(&mut m, cur_row, cur_col, &mut rng);
+        let next_point = fill_next_point(&mut m, cur_point, &mut rng);
 
-        cur_row = next_row;
-        cur_col = next_col;
-        // println!("Next row is {}, col is {}", cur_row, cur_col);
+        fill_walls(&mut m, cur_point);
+
+        cur_point = next_point;
     }
 
     return m
 
 }
 
-fn fill_next_point(m: &mut Matrix<Point>, cur_row: usize, cur_col: usize, rng: &mut ThreadRng) -> (usize, usize) {
-
-    let (cur_row, cur_col) = (cur_row as i32, cur_col as i32);
+fn fill_next_point(m: &mut Matrix<MazeObj>, cur_point: Point, rng: &mut ThreadRng) -> Point {
 
     let cardinal_points = vec![
-        (cur_row - 1, cur_col), //North
-        (cur_row + 1, cur_col), //South
-        (cur_row, cur_col - 1), //West
-        (cur_row, cur_col + 1)  //East
+        cur_point.translate(PointDelta{dx: -1, dy:  0}), //North
+        cur_point.translate(PointDelta{dx:  1, dy:  0}), //South
+        cur_point.translate(PointDelta{dx:  0, dy: -1}), //West
+        cur_point.translate(PointDelta{dx:  0, dy:  1}), //East
     ];
 
-    let valid_points = cardinal_points.into_iter().filter(|&p| is_valid_point(&m, p));
+    let valid_points = cardinal_points
+        .into_iter()
+        .filter(|op| match op {
+            Some(p) => is_valid_point(m, p),
+            None => false
+        }).flatten();
 
-    // println!("total vp {}", &valid_points.count());
     
     match valid_points.choose(rng) {
         Some(point) => {
             // println!("Prev row is {}, col is {}", cur_row, cur_col);
-            let (new_row, new_col) = point;
-            m.set(new_row as usize, new_col as usize, Point::Path);
-            (new_row as usize, new_col as usize)
+            m.set(point, MazeObj::Path);
+            point
         },
         None => {
             println!("Oh no!");
-            (cur_row as usize, cur_col as usize)
+            cur_point
         } 
     }
 
 }
 
-fn is_valid_point(m: &Matrix<Point>, point: (i32, i32)) -> bool {
+fn fill_walls(m: &mut Matrix<MazeObj>, point: Point) {
+
+    let surrounding_points = vec![
+        point.translate(PointDelta{dx: -1, dy:  0}), //North
+        point.translate(PointDelta{dx:  1, dy:  0}), //South
+        point.translate(PointDelta{dx:  0, dy: -1}), //West
+        point.translate(PointDelta{dx:  0, dy:  1}), //East
+
+        point.translate(PointDelta{dx: -1, dy: -1}), //North West
+        point.translate(PointDelta{dx: -1, dy:  1}), //North East
+        point.translate(PointDelta{dx:  1, dy: -1}), //South West
+        point.translate(PointDelta{dx:  1, dy:  1}), //South East
+
+        // (row - 1, col), //North
+        // (row + 1, col), //South
+        // (row, col - 1), //West
+        // (row, col + 1),  //East
+        // (row - 1, col - 1), //North West
+        // (row - 1, col + 1), //North East
+        // (row + 1, col - 1), //South West
+        // (row + 1, col + 1), //South East
+    ];
+
+    for sp in surrounding_points {
+        if let Some(p) = sp {
+            if is_valid_point(&m, &p) {
+                m.set(p, MazeObj::Wall);
+            }
+        }
+    }
+
+}
+
+fn is_valid_point(m: &Matrix<MazeObj>, point: &Point) -> bool {
     let (total_rows, total_cols) = m.shape();
-    let (total_rows, total_cols) = (total_rows as i32, total_cols as i32);
 
     match point {
-        (row, _) if row > total_rows - 1 => false,
-        (_, col) if col > total_cols - 1 => false,
-        (row, _) if row < 0 => false,
-        (_, col) if col < 0 => false,
-        (row, col) if matches!(m.at(row as usize, col as usize),Point::Blank) => true,
-        (_, _) => false
+        Point{x, y} if (
+            x <= &(total_rows - 1) && y <= &(total_cols - 1) && matches!(m.at(point), MazeObj::Blank)
+        ) => true,
+        _ => false
     }
 }
 
@@ -78,14 +106,13 @@ fn main() {
     // let mut m = Matrix::new(2, 5);
     // m.set(0, 4, Point::Path);
 
-    let mut m = generate_path(10,10,5);
-
+    let m = generate_path(10,10,5);
     let mut stdout = stdout();
     stdout.queue(cursor::SavePosition).ok();
     m.render(&mut stdout);
     stdout.queue(cursor::RestorePosition).ok();
     stdout.flush().ok();
-    // thread::sleep(time::Duration::from_millis(5000));
+    thread::sleep(time::Duration::from_millis(5000));
 
     // for i in 1..10 {
     //     stdout.queue(cursor::SavePosition).ok();
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,10 +1,60 @@
-use std::io::{Stdout, Write, stdout};
+use std::io::{Stdout, Write};
+
+#[derive(Debug,  Copy, Clone, Hash, Eq, PartialEq,)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct PointDelta {
+    pub dx: isize,
+    pub dy: isize
+}
+
+
+
+impl Point {
+
+    pub fn translate(self, delta: PointDelta) -> Option<Point> {
+        let PointDelta{dx, dy} = delta;
+        let Point{x, y} = self;
+
+        match ((x as isize).checked_add(dx), (y as isize).checked_add(dy)) {
+            (Some(new_x), Some(new_y)) if new_x >= 0 && new_y >= 0 => Some(Point{x: new_x as usize, y: new_y as usize}),
+            (_ , _) => None
+        }
+    }
+
+    // pub fn add(self, other: Point) -> Option<Point> {
+    //     Point::checked_math(usize::checked_add, self, other)
+    // }   
+
+    // pub fn sub(self, other: Point) -> Option<Point> {
+    //     Point::checked_math(usize::checked_sub, self, other)
+    // }
+
+    // fn checked_math(f: fn(usize, usize) -> Option<usize>, s: Point, o: Point) ->  Option<Point> {
+    //     match (f(s.x, o.x) , f(s.y, o.y)) {
+    //         (Some(x), Some(y)) => Some(Point{x, y}),
+    //         (_ , _) => None
+    //     }
+    // }
+
+    // pub fn sub(self, other: Point) -> Option<Point> {
+    //     match (self.x.checked_sub(other.x) , self.y.checked_sub(other.y)) {
+    //         (Some(x), Some(y)) => Some(Point{x, y}),
+    //         (_ , _) => None
+    //     }
+    // }
+
+    
+}
 
 pub struct Matrix<T> {
     vals: Vec<Vec<T>>,
 }
 
-trait MatrixValue {
+pub trait MatrixValue {
     fn default_val() -> Self;
     fn as_char(&self) -> &'static str;
 }
@@ -30,12 +80,12 @@ where T: MatrixValue + Copy
         }
     }
 
-    pub fn at(&self, row: usize, col: usize) -> T {
-        self.vals[row][col]
+    pub fn at(&self, p: &Point) -> T {
+        self.vals[p.x][p.y]
     }
 
-    pub fn set(&mut self, row: usize, col: usize, val: T) {
-        self.vals[row][col] = val
+    pub fn set(&mut self, p: Point, val: T) {
+        self.vals[p.x][p.y] = val
     }
 
     pub fn render(&self, stdout: &mut Stdout) {
@@ -49,32 +99,32 @@ where T: MatrixValue + Copy
         }
     }
 
-    pub fn add_row(&mut self) {
-        let (_, cols) =  self.shape();
-        println!("{}", cols);
+    // pub fn add_row(&mut self) {
+    //     let (_, cols) =  self.shape();
+    //     println!("{}", cols);
 
-        self.vals.push(vec![T::default_val(); cols]);
-    }
+    //     self.vals.push(vec![T::default_val(); cols]);
+    // }
 
 }
 
 #[derive(Copy, Clone)]
-pub enum Point {
+pub enum MazeObj {
     Blank,
     Path,
     Wall,
 }
 
-impl MatrixValue for Point {
-    fn default_val() -> Point {
-        Point::Blank
+impl MatrixValue for MazeObj {
+    fn default_val() -> MazeObj {
+        MazeObj::Blank
     }
 
     fn as_char(&self) -> &'static str {
         match self {
-            Point::Blank => " ",
-            Point::Path =>  ".",
-            Point::Wall => "X",
+            MazeObj::Blank => "#",
+            MazeObj::Path =>  ".",
+            MazeObj::Wall => "X",
         }
     }  
 }
